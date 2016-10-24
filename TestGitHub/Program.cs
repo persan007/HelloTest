9d21592e7749c20568e7dd99641cefa0ecb14ac5
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-24 11:29:06 +0200

    New writeline

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -11,6 +11,7 @@ namespace TestGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello again");
             Console.ReadKey();
         }
     }
