07634023e948b04aa9ea659f4803668c23995eb8
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-24 15:03:30 +0200

    Nytt meddelande

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -10,11 +10,16 @@ namespace TestGitHub
     {
         static void Main(string[] args)
         {
+            
+            //New branch
+            Dog dogy = new Dog("Bulldog");
+            dogy.Name = "Tax";
+            
             List<Dog> dogs = new List<Dog>();
             dogs.Add(new Dog("Fido"));
             dogs.Add(new Dog("Bob"));
             dogs.Add(new Dog("Adam"));
-            
+            dogs.Add(dogy);
 
             foreach (Dog dog in dogs)
                 Console.WriteLine(dog.Describe());
